be6c1786c3da005ee0b83ef1f03fd7452c70b5ab
monidipbose <monidip.bose7@gmail.com>
2020-09-07 00:01:07 +0530

    new method GetPromotion added to check and get appropriate promotion for the item

--- PromotionEngine.Tests/CalculateTotal.Tests.cs
+++ PromotionEngine.Tests/CalculateTotal.Tests.cs
@@ -4,8 +4,23 @@ using Xunit;
 
 namespace PromotionEngine.Tests
 {
-    public class CalculateTotalTests
+    public class CalculateTotalFixture
     {
+        public CalculateTotal calculateTotal => new CalculateTotal();
+        public List<Item> items = data.Items;
+        public List<Promotion> promotions = data.Promotions;
+        public Cart cart1 = data.Cart1;
+        public Cart cart2 = data.Cart2;
+        public Cart cart3 = data.Cart3;
+    }
+    public class CalculateTotalTests : IClassFixture<CalculateTotalFixture>
+    {
+        private CalculateTotalFixture _calculateTotalFixture;
+        public CalculateTotalTests(CalculateTotalFixture calculateTotalFixture)
+        {
+            _calculateTotalFixture = calculateTotalFixture;
+        }
+
         [Fact]
         public void GetTotal_GivenItemsPromotionsCart1Values_ReturnTotalCartValueAsInt()
         {
@@ -16,6 +31,25 @@ namespace PromotionEngine.Tests
             int expectedTotal = 100;
             Assert.Equal(expectedTotal, calculateTotal
                 .GetTotal(items, promotions, cart1));
+        }
+
+        [Fact]
+        public void GetPromotion_GivenItemNameAndPromotions_ReturnAvailablePromotionForThisItem()
+        {
+            var calculateTotal = _calculateTotalFixture.calculateTotal;
+            char itemName = 'A';
+            var result = calculateTotal.GetPromotion(itemName, _calculateTotalFixture.promotions);
+            Assert.NotNull(result);
+            Assert.IsType<Promotion>(result);
+            Assert.Equal(130, result.Price);
+        }
+        [Fact]
+        public void GetPromotion_GivenItemNameAndPromotions_ReturnNullAsNoPromotionAvailableForThisItem()
+        {
+            var calculateTotal = _calculateTotalFixture.calculateTotal;
+            char itemName = 'E';
+            var result = calculateTotal.GetPromotion(itemName, _calculateTotalFixture.promotions);
+            Assert.Null(result);
         }
     }
 }
--- PromotionEngine/CalculateTotal.cs
+++ PromotionEngine/CalculateTotal.cs
@@ -8,7 +8,39 @@ namespace PromotionEngine
     {
         public int GetTotal(List<Item> items, List<Promotion> promotions, Cart cart)
         {
-            return 0;
+            List<char> promoAppliedItems = new List<char>();
+            int total = 0;
+            int i = 0;
+            foreach (var item in cart.Items)
+            {
+                int itemPrice = items.FirstOrDefault(a => a.Name == item).Price;
+                if (!promoAppliedItems.Contains(item))
+                {
+                    var promo = GetPromotion(item, promotions);
+                    if (promo != null)
+                    {
+                        
+                    }
+                    else
+                    {
+                        total += (cart.Quantity[i] * itemPrice);
+                    }
+                }
+                i++;
+            }
+            return total;
+        }
+
+        public Promotion? GetPromotion(char item, List<Promotion> promotions)
+        {
+            foreach (var promotion in promotions)
+            {
+                if (promotion.Items.Contains(item))
+                {
+                    return promotion;
+                }
+            }
+            return null;
         }
     }
 }
